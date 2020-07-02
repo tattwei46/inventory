1ee4d2c269aa0c1b3ed836f6580ba131a80666a6
DAVID CHEAH <david.cheah@cashshield.com>
2020-07-02 17:06:00 +0800

    Client update

--- client/inventory/src/components/Inventory.js
+++ client/inventory/src/components/Inventory.js
@@ -12,10 +12,32 @@ import EditIcon from "@material-ui/icons/Edit";
 import Button from "@material-ui/core/Button";
 import Fab from "@material-ui/core/Fab";
 import AddIcon from "@material-ui/icons/Add";
+import SearchIcon from "@material-ui/icons/Search";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import TextField from "@material-ui/core/TextField";
+import DateFnsUtils from "@date-io/date-fns";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from "@material-ui/pickers";
+import Path from "./Path";
+import moment from "moment";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
 
 const axios = require("axios");
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
   table: {
     minWidth: 650,
   },
@@ -23,17 +45,45 @@ const useStyles = makeStyles((theme) => ({
     position: "absolute",
     right: theme.spacing(2),
   },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
 }));
 
 const Inventory = (props) => {
   const classes = useStyles();
-  const theme = useTheme();
 
   const [assets, setAssets] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [search, setSearch] = useState(false);
+  const [update, setUpdate] = useState(false);
+
+  const [id, setId] = useState("");
+  const [serialNumber, setSerialNumber] = useState("");
+  const [brand, setBrand] = useState("");
+  const [model, setModel] = useState("");
+  const [status, setStatus] = useState("");
+
+  const handleStatusChange = (event) => {
+    setStatus(event.target.value);
+  };
+
+  const [selectedDate, setSelectedDate] = React.useState(
+    new Date("2020-07-01T00:00:00")
+  );
+
+  const [selectedFrom, setSelectedFrom] = React.useState(
+    new Date("2020-07-01T00:00:00")
+  );
+
+  const [selectedTo, setSelectedTo] = React.useState(
+    new Date("2020-07-01T00:00:00")
+  );
 
   useEffect(() => {
     axios
-      .get("/inventory/v1/assets", {})
+      .get("/inventory/v1/assets")
       .then(function (response) {
         if (response.data) {
           setAssets(response.data);
@@ -42,18 +92,134 @@ const Inventory = (props) => {
       .catch(function (error) {
         console.log(error);
       });
-  });
+  }, [open]);
 
-  const handleOnEdit = (id) => {
-    console.log(id);
+  const handleOnDateChange = (date) => {
+    setSelectedDate(date);
   };
 
-  const handleOnDelete = (id) => {
-    console.log(id);
+  const handleOnFromChange = (date) => {
+    setSelectedFrom(date);
+  };
+
+  const handleOnToChange = (date) => {
+    setSelectedTo(date);
+  };
+
+  const handleOnOpen = () => {
+    setOpen(true);
+  };
+
+  const handleOnSearchOpen = () => {
+    setSearch(true);
+  };
+
+  const handleOnClose = () => {
+    if (update) setUpdate(false);
+    setOpen(false);
+  };
+
+  const handleOnSearchClose = () => {
+    setSearch(false);
+  };
+
+  const handleOnSearch = () => {
+    var range = {
+      from: moment(selectedFrom).format("YYYY-MM-DD"),
+      to: moment(selectedTo).format("YYYY-MM-DD"),
+    };
+
+    axios
+      .post("/inventory/v1/assets/action/search", {
+        range: range,
+        model: model,
+        brand: brand,
+        serial_number: serialNumber,
+        status: status,
+      })
+      .then(function (response) {
+        if (response.status === 200) {
+          setAssets(response.data);
+        }
+        if (response.status === 204) {
+          setAssets([]);
+        }
+
+        setSearch(false);
+      })
+      .catch(function (error) {
+        console.log(error);
+        setSearch(false);
+      });
   };
 
   const handleOnAdd = () => {
-    console.log("add");
+    if (update) {
+      axios
+        .put("/inventory/v1/assets/" + id, {
+          created: moment(selectedDate).format("YYYY-MM-DD"),
+          model: model,
+          brand: brand,
+          serial_number: serialNumber,
+          status: status,
+        })
+        .then(function (response) {
+          if (response.status === 200) {
+            setUpdate(false);
+            setOpen(false);
+          }
+        })
+        .catch(function (error) {
+          console.log(error);
+          setUpdate(false);
+          setOpen(false);
+        });
+    } else {
+      axios
+        .post("/inventory/v1/assets", {
+          created: selectedDate,
+          model: model,
+          brand: brand,
+          serial_number: serialNumber,
+        })
+        .then(function (response) {
+          if (response.status === 200) {
+            setOpen(false);
+          }
+        })
+        .catch(function (error) {
+          console.log(error);
+          setOpen(false);
+        });
+    }
+  };
+
+  const handleOnDelete = (id) => {
+    axios
+      .delete("/inventory/v1/assets/" + id)
+      .then(function (response) {
+        if (response.status === 200) {
+          // TODO : HOW TO HANDLE IT MORE GRACEFULLY
+          window.location = Path.Inventory;
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+        // TODO : HOW TO HANDLE IT MORE GRACEFULLY
+        window.location = Path.Inventory;
+      });
+  };
+
+  const handleOnEdit = (asset) => {
+    setId(asset.id);
+    setSelectedDate(asset.created);
+    setBrand(asset.brand);
+    setModel(asset.model);
+    setStatus(asset.status);
+    setSerialNumber(asset.serial_number);
+
+    setUpdate(true);
+    setOpen(true);
   };
 
   return (
@@ -84,7 +250,7 @@ const Inventory = (props) => {
                 <TableCell align="left">{row.serial_number}</TableCell>
                 <TableCell align="left">{row.status}</TableCell>
                 <TableCell align="left">
-                  <Button onClick={() => handleOnEdit(row.id)}>
+                  <Button onClick={() => handleOnEdit(row)}>
                     <EditIcon />
                   </Button>
                 </TableCell>
@@ -99,14 +265,197 @@ const Inventory = (props) => {
         </Table>
       </TableContainer>
 
-      <Fab
-        color="primary"
-        aria-label="add"
-        className={classes.fab}
-        onClick={handleOnAdd}
+      <div className={classes.fab}>
+        {/*Floating Action Button to Add Items*/}
+        <Fab color="primary" aria-label="add" onClick={handleOnOpen}>
+          <AddIcon />
+        </Fab>
+        {/*Floating Action Button to Search Items*/}
+        <Fab color="secondary" aria-label="edit" onClick={handleOnSearchOpen}>
+          <SearchIcon />
+        </Fab>
+      </div>
+
+      {/*Form Dialog to Add Items*/}
+      <Dialog
+        open={open}
+        onClose={handleOnClose}
+        aria-labelledby="form-dialog-title"
       >
-        <AddIcon />
-      </Fab>
+        <DialogTitle id="form-dialog-title">
+          {update ? "Update" : "Add"} Items
+        </DialogTitle>
+        <DialogContent>
+          <div>
+            {update ? (
+              <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <KeyboardDatePicker
+                  disableToolbar
+                  variant="inline"
+                  format="MM/dd/yyyy"
+                  margin="normal"
+                  id="date-picker-inline"
+                  label="Date"
+                  value={selectedDate}
+                  onChange={handleOnDateChange}
+                  KeyboardButtonProps={{
+                    "aria-label": "change date",
+                  }}
+                />
+              </MuiPickersUtilsProvider>
+            ) : null}
+          </div>
+
+          <TextField
+            required
+            autoFocus
+            id="brand"
+            label="Brand"
+            type="text"
+            fullWidth
+            value={update ? brand : null}
+            onChange={(e) => setBrand(e.target.value)}
+          />
+          <TextField
+            required
+            autoFocus
+            id="model"
+            label="Model"
+            type="text"
+            fullWidth
+            value={update ? model : null}
+            onChange={(e) => setModel(e.target.value)}
+          />
+          <TextField
+            required
+            autoFocus
+            id="serial_number"
+            label="Serial Number"
+            type="text"
+            fullWidth
+            value={update ? serialNumber : null}
+            onChange={(e) => setSerialNumber(e.target.value)}
+          />
+          {update ? (
+            <FormControl className={classes.formControl}>
+              <InputLabel id="demo-simple-select-label">Status</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={update ? status : null}
+                onChange={handleStatusChange}
+              >
+                <MenuItem value={"Available"}>Available</MenuItem>
+                <MenuItem value={"Not Available"}>Not Available</MenuItem>
+              </Select>
+            </FormControl>
+          ) : null}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleOnClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleOnAdd} color="primary">
+            {update ? `Update` : `Add`}
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      {/*Form Dialog to Search Items*/}
+      <Dialog
+        open={search}
+        onClose={handleOnSearchClose}
+        aria-labelledby="form-dialog-title"
+      >
+        <DialogTitle id="form-dialog-title-search">Search Items</DialogTitle>
+        <DialogContent>
+          <div>
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+              <KeyboardDatePicker
+                disableToolbar
+                variant="inline"
+                format="MM/dd/yyyy"
+                margin="normal"
+                id="date-picker-inline"
+                label="From"
+                value={selectedFrom}
+                onChange={handleOnFromChange}
+                KeyboardButtonProps={{
+                  "aria-label": "change date",
+                }}
+              />
+            </MuiPickersUtilsProvider>
+
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+              <KeyboardDatePicker
+                disableToolbar
+                variant="inline"
+                format="MM/dd/yyyy"
+                margin="normal"
+                id="date-picker-inline"
+                label="To"
+                value={selectedTo}
+                onChange={handleOnToChange}
+                KeyboardButtonProps={{
+                  "aria-label": "change date",
+                }}
+              />
+            </MuiPickersUtilsProvider>
+          </div>
+
+          <TextField
+            required
+            autoFocus
+            id="brand"
+            label="Brand"
+            type="text"
+            fullWidth
+            value={update ? brand : null}
+            onChange={(e) => setBrand(e.target.value)}
+          />
+          <TextField
+            required
+            autoFocus
+            id="model"
+            label="Model"
+            type="text"
+            fullWidth
+            value={update ? model : null}
+            onChange={(e) => setModel(e.target.value)}
+          />
+          <TextField
+            required
+            autoFocus
+            id="serial_number"
+            label="Serial Number"
+            type="text"
+            fullWidth
+            value={update ? serialNumber : null}
+            onChange={(e) => setSerialNumber(e.target.value)}
+          />
+
+          <FormControl className={classes.formControl}>
+            <InputLabel id="demo-simple-select-label">Status</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={status}
+              onChange={handleStatusChange}
+            >
+              <MenuItem value={"Available"}>Available</MenuItem>
+              <MenuItem value={"Not Available"}>Not Available</MenuItem>
+            </Select>
+          </FormControl>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleOnSearchClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleOnSearch} color="primary">
+            Search
+          </Button>
+        </DialogActions>
+      </Dialog>
     </React.Fragment>
   );
 };
